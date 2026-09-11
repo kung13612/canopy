a33ec10cedaf104a695feec07f2784aa17c3288a
kung13612 <49513749+kung13612@users.noreply.github.com>
2026-09-11 08:04:10 +0700

    Part3h wallet src types

--- global.d.ts
+++ global.d.ts
@@ -1,15 +1,12 @@
-// Global type declarations
-
-// Extend Window interface to include __CONFIG__
 declare global {
     interface Window {
         __CONFIG__?: {
             rpcURL: string;
             adminRPCURL: string;
+            explorerBaseURL?: string;
             chainId: number;
         };
     }
 }
 
-// Export to make it a module
 export { };
